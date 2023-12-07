f7cbfe9805d08e72b067e61e90a8b431455ee987
DawidRempinski <dawid@rempinski.de>
2023-12-07 17:06:17 +0100

    v5

--- setup.sh
+++ setup.sh
@@ -16,7 +16,7 @@ echo "\
 [theme]\n\
 primaryColor = '#7792E3'\n\
 backgroundColor = '#e6e6e6'\n\
-secondaryBackgroundColor = '#1ed760'\n\
+secondaryBackgroundColor = '#7beaa2'\n\
 textColor = '#000000'\n\
 font = 'sans serif'\n\
 " >> ~/.streamlit/config.toml
