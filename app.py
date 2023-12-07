5cf462ea16f7a6c003659c8f52ede92a092ec2ee
DawidRempinski <dawid@rempinski.de>
2023-12-07 11:53:26 +0100

    several changes

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import pickle
 from sklearn.preprocessing import RobustScaler
 import joblib
 import numpy as np
+import time
 
 # Spotify API-Funktionen
 
@@ -105,190 +106,210 @@ else:
 
 # Schritt 2: Anzeigen der Track-Informationen und Audio-Features
 if selected_track_id:
-    # Hole Track-Informationen und Audio-Features für den ausgewählten Track
-    track_data, audio_features = get_track_info_and_features(selected_track_id, spotify_access_token)
+    # Display the prediction button
+    predict_button = st.button("Predict")
+    # Continue with predictions only if the Predict button is clicked
+    if predict_button:
+        progress_text = "The prediction is being calculated... Please wait."
+        my_bar = st.progress(0, text=progress_text)
 
-    if track_data and audio_features:
-        # Extrahiere die Vorschau-URL des Tracks
-        preview_url = track_data.get('preview_url', None)
-        
-        if preview_url:
-            st.subheader("Audio Preview:")
-            st.audio(preview_url, format="audio/mp3", start_time=0)
-        else:
-            st.warning("No audio preview available for this track.")
+        for percent_complete in range(100):
+            time.sleep(0.015)
+            my_bar.progress(percent_complete + 1, text=progress_text)
+        time.sleep(0.5)
+        my_bar.empty()
+        # Hole Track-Informationen und Audio-Features für den ausgewählten Track
+        track_data, audio_features = get_track_info_and_features(selected_track_id, spotify_access_token)
+
+        if track_data and audio_features:
+                         
+            # Extrahiere die numerischen Features
+            danceability = audio_features['danceability']
+            energy = audio_features['energy']
+            loudness = audio_features['loudness']
+            speechiness = audio_features['speechiness']
+            acousticness = audio_features['acousticness']
+            liveness = audio_features['liveness']
+            valence = audio_features['valence']
+            tempo = audio_features['tempo']
+            duration_ms = audio_features['duration_ms']
+            instrumentalness = audio_features['instrumentalness']
+
+            # Release Year aus dem Release Date extrahieren
+            release_date = track_data['album']['release_date']
+            release_year = pd.to_datetime(release_date).year
+
+            # Erstelle ein DataFrame für numerische Features
+            numeric_features_data = pd.DataFrame({
+                'danceability': [danceability],
+                'energy': [energy],
+                'loudness': [loudness],
+                'speechiness': [speechiness],
+                'acousticness': [acousticness],
+                'liveness': [liveness],
+                'valence': [valence],
+                'tempo': [tempo],
+                'duration_ms': [duration_ms],
+                'release_year': [release_year],
+                'instrumentalness': [instrumentalness]
+            })
+
+            # Extrahiere das Genre des Künstlers über einen separaten API-Aufruf
+            artist_id = track_data['artists'][0]['id']
+            artist_info_url = f'https://api.spotify.com/v1/artists/{artist_id}'
+            artist_headers = {'Authorization': f'Bearer {spotify_access_token}'}
+
+            try:
+                artist_response = requests.get(artist_info_url, headers=artist_headers)
+                artist_response.raise_for_status()
+                artist_info = artist_response.json()
+
+                # Extrahiere das Genre des Künstlers
+                artist_genres = artist_info.get('genres', [])  # Liste der Künstler-Genres
+
+            except requests.exceptions.RequestException as e:
+                st.error(f"Error getting artist information for artist ID {artist_id}: {e}")
+                artist_genres = []
+
+            # st.subheader("Track Information:")
+            # st.write(f"Name: {track_data['name']}")
+            # st.write(f"Artist: {track_data['artists'][0]['name']}")
+            # st.write(f"Popularity: {track_data['popularity']}")
+            # st.write(f"Release Date: {track_data['album']['release_date']}")
+            # st.write(f"Track ID: {track_data['id']}")
+            # st.write(f"Artist genres: {artist_genres}")
+
+            # DataFrame für bekannte Genres erstellen
+            prediction_data = pd.DataFrame(columns=[
+                'genres_Afro', 'genres_Alternative', 'genres_Ambient', 'genres_Blues',
+                'genres_Christian & Gospel', 'genres_Classical', 'genres_Country',
+                'genres_Dance/Electronic', 'genres_Folk & Acoustic', 'genres_Hip-Hop',
+                'genres_Instrumental', 'genres_Indie', 'genres_Jazz', 'genres_K-Pop', 'genres_Latin',
+                'genres_Metal', 'genres_Pop', 'genres_Punk', 'genres_R&B', 'genres_Rock',
+                'genres_Soul'
+            ])
+
+            # Überall 0 setzen
+            prediction_data.loc[0] = 0
+
+            # Prüfe, ob ein Künstler-Genre zu den Spaltennamen passt, und setze die entsprechende Spalte auf 1
+            for genre in artist_genres:
+                lowercase_genre = genre.lower()
+
+                # Suche nach exakten Übereinstimmungen in den normalisierten Spaltennamen
+                matching_columns = [col for col in prediction_data.columns if lowercase_genre == col.replace('genres_', '').lower()]
+
+                # Prüfe zusätzlich auf "hip hop" und "rap" als Teil des Genre-Namens
+                if "hip hop" in lowercase_genre or "rap" in lowercase_genre:
+                    matching_columns.extend([col for col in prediction_data.columns if "hip-hop" in col.replace('genres_', '').lower() or "rap" in col.replace('genres_', '').lower()])
+
+                # Debug-Ausgabe für die gefundenen und verglichenen Spalten
+                print(f"Genre: {lowercase_genre}, Matching Columns: {matching_columns}")
+
+                if matching_columns:
+                    # Setze die entsprechende Spalte auf 1, aber nur für "K-Pop" (nicht "Pop"), "Hip Hop" und "Rap"
+                    if lowercase_genre == "k-pop":
+                        prediction_data.at[0, matching_columns[0]] = 1
+                    elif "pop" in matching_columns[0].lower():
+                        # Setze die "Pop"-Spalte nur, wenn das Genre nicht "K-Pop" ist
+                        prediction_data.at[0, matching_columns[0]] = 1
+                    elif "hip-hop" in matching_columns[0].lower() or "rap" in matching_columns[0].lower():
+                        # Setze die "Hip Hop" und "Rap"-Spalten nur, wenn das Genre "hip hop" oder "rap" ist
+                        prediction_data.at[0, matching_columns[0]] = 1
+
             
-        # Extrahiere die numerischen Features
-        danceability = audio_features['danceability']
-        energy = audio_features['energy']
-        loudness = audio_features['loudness']
-        speechiness = audio_features['speechiness']
-        acousticness = audio_features['acousticness']
-        liveness = audio_features['liveness']
-        valence = audio_features['valence']
-        tempo = audio_features['tempo']
-        duration_ms = audio_features['duration_ms']
-        instrumentalness = audio_features['instrumentalness']
+            # st.subheader("Track Information:")
+            # st.write(f"Name: {track_data['name']}")
+            # st.write(f"Artist: {track_data['artists'][0]['name']}")
+            # st.write(f"Popularity: {track_data['popularity']}")
+            # st.write(f"Release Date: {track_data['album']['release_date']}")
+            # st.write(f"Track ID: {track_data['id']}")
 
-        # Release Year aus dem Release Date extrahieren
-        release_date = track_data['album']['release_date']
-        release_year = pd.to_datetime(release_date).year
+            #st.subheader("Audio Features:")
+            #st.write(audio_features)
 
-        # Erstelle ein DataFrame für numerische Features
-        numeric_features_data = pd.DataFrame({
-            'danceability': [danceability],
-            'energy': [energy],
-            'loudness': [loudness],
-            'speechiness': [speechiness],
-            'acousticness': [acousticness],
-            'liveness': [liveness],
-            'valence': [valence],
-            'tempo': [tempo],
-            'duration_ms': [duration_ms],
-            'release_year': [release_year],
-            'instrumentalness': [instrumentalness]
-        })
+            # Laden Sie die gespeicherten Scaler-Parameter
+            with open('scaler.joblib', 'rb') as file:
+                scaler = joblib.load(file)
 
+            # Skaliere die numerischen Features
+            scaled_numeric_features = scaler.transform(numeric_features_data)
+            scaled_numeric_features_df = pd.DataFrame(scaled_numeric_features, columns=numeric_features_data.columns)
 
-        # DataFrame für bekannte Genres erstellen
-        prediction_data = pd.DataFrame(columns=[
-            'genres_Afro', 'genres_Alternative', 'genres_Ambient', 'genres_Blues',
-            'genres_Christian & Gospel', 'genres_Classical', 'genres_Country',
-            'genres_Dance/Electronic', 'genres_Folk & Acoustic', 'genres_Hip-Hop',
-            'genres_Instrumental', 'genres_Jazz', 'genres_K-Pop', 'genres_Latin',
-            'genres_Metal', 'genres_Pop', 'genres_Punk', 'genres_R&B', 'genres_Rock',
-            'genres_Soul'
-        ])
+            # Hier erfolgt die Verknüpfung
+            final_data = pd.concat([scaled_numeric_features_df, prediction_data], axis=1)
 
-        # Überall 0 setzen
-        prediction_data.loc[0] = 0
+            df = pd.DataFrame(tracks_data)
+            # st.write("Collected Data:")
+            # st.write(final_data)
 
-        # Extrahiere das Genre des Künstlers über einen separaten API-Aufruf
-        artist_id = track_data['artists'][0]['id']
-        artist_info_url = f'https://api.spotify.com/v1/artists/{artist_id}'
-        artist_headers = {'Authorization': f'Bearer {spotify_access_token}'}
+            # Laden Sie das trainierte Modell
+            with open('model1.pkl', 'rb') as file:
+                trained_model = pickle.load(file)
 
-        try:
-            artist_response = requests.get(artist_info_url, headers=artist_headers)
-            artist_response.raise_for_status()
-            artist_info = artist_response.json()
+            # Extrahiere die Features für die Vorhersage
+            features_for_prediction = final_data.values.reshape(1, -1)  # Reshape für eine einzelne Beobachtung
 
-            # Extrahiere das Genre des Künstlers
-            artist_genres = artist_info.get('genres', [])  # Liste der Künstler-Genres
+            # Führe die Vorhersage durch
+            predicted_score = trained_model.predict(features_for_prediction)
 
-        except requests.exceptions.RequestException as e:
-            st.error(f"Error getting artist information for artist ID {artist_id}: {e}")
-            artist_genres = []
+            # Lade das zweite Modell
+            loaded_model = pickle.load(open("monthly_listeners_model.pkl", "rb"))
 
-        # st.subheader("Track Information:")
-        # st.write(f"Name: {track_data['name']}")
-        # st.write(f"Artist: {track_data['artists'][0]['name']}")
-        # st.write(f"Popularity: {track_data['popularity']}")
-        # st.write(f"Release Date: {track_data['album']['release_date']}")
-        # st.write(f"Track ID: {track_data['id']}")
-        # st.write(f"Artist genres: {artist_genres}")
+            # Annahme: predicted_score ist der vorhergesagte Wert
+            pred_score = predicted_score
 
-        # DataFrame für bekannte Genres erstellen
-        prediction_data = pd.DataFrame(columns=[
-            'genres_Afro', 'genres_Alternative', 'genres_Ambient', 'genres_Blues',
-            'genres_Christian & Gospel', 'genres_Classical', 'genres_Country',
-            'genres_Dance/Electronic', 'genres_Folk & Acoustic', 'genres_Hip-Hop',
-            'genres_Instrumental', 'genres_Indie', 'genres_Jazz', 'genres_K-Pop', 'genres_Latin',
-            'genres_Metal', 'genres_Pop', 'genres_Punk', 'genres_R&B', 'genres_Rock',
-            'genres_Soul'
-        ])
+            # Führe die Vorhersage durch
+            predicted_monthly_listeners = loaded_model.predict([pred_score])
 
-        # Überall 0 setzen
-        prediction_data.loc[0] = 0
+            # Berechne zusätzliche Metriken basierend auf der Vorhersage
+            predicted_monthly_streams = predicted_monthly_listeners * 2.5
+            predicted_monthly_revenue = predicted_monthly_streams * 0.004
 
-        # Prüfe, ob ein Künstler-Genre zu den Spaltennamen passt, und setze die entsprechende Spalte auf 1
-        for genre in artist_genres:
-            lowercase_genre = genre.lower()
+            # Jetzt hast du die vorhergesagten Werte, die du in deinem Hauptcode verwenden kannst
+            st.header("Prediction results:")
 
-            # Suche nach exakten Übereinstimmungen in den normalisierten Spaltennamen
-            matching_columns = [col for col in prediction_data.columns if lowercase_genre == col.replace('genres_', '').lower()]
+            predicted_score_text = f"Predicted Popularity Score for the song: {int(round(predicted_score[0])):,}"
+            predicted_monthly_listeners_text = f"**Predicted Monthly Listeners:** {int(round(predicted_monthly_listeners[0])):,}"
+            predicted_monthly_streams_text = f"**Predicted Monthly Streams:** {int(round(predicted_monthly_streams[0])):,}"
+            predicted_monthly_revenue_text = f"Predicted Monthly Revenue for the artist of the song: ${round(predicted_monthly_revenue[0], 2):,}"
 
-            # Prüfe zusätzlich auf "hip hop" und "rap" als Teil des Genre-Namens
-            if "hip hop" in lowercase_genre or "rap" in lowercase_genre:
-                matching_columns.extend([col for col in prediction_data.columns if "hip-hop" in col.replace('genres_', '').lower() or "rap" in col.replace('genres_', '').lower()])
+            # Verwende 'success' für positive Ergebnisse
+            # Wenn kein passendes Genre gefunden wurde, gib eine Warnung aus
+            if prediction_data.iloc[0].sum() == 0:
+                st.error("Be careful with the result! It could be inaccurate because no genre was found for the song that fits the prediction model.", icon="🚨")
 
-            # Debug-Ausgabe für die gefundenen und verglichenen Spalten
-            print(f"Genre: {lowercase_genre}, Matching Columns: {matching_columns}")
+            with st.container():
+                # Füge die größere und auffälligere Ausgabe hinzu
+                st.markdown(
+                    f"<h2 style='color: #1DB954; font-size: 30px;'>{predicted_score_text}</h2>", 
+                    unsafe_allow_html=True
+                )
+                # st.markdown(
+                #     f"<p style='font-size: 20px;'>{predicted_monthly_listeners_text}</p>",
+                #     unsafe_allow_html=True
+                # )
+                # st.markdown(
+                #     f"<p style='font-size: 20px;'>{predicted_monthly_streams_text}</p>",
+                #     unsafe_allow_html=True
+                # )
+                st.markdown(
+                    f"<h2 style='color: #1DB954; font-size: 30px;'>{predicted_monthly_revenue_text}</h2>", 
+                    unsafe_allow_html=True
+                )
 
-            if matching_columns:
-                # Setze die entsprechende Spalte auf 1, aber nur für "K-Pop" (nicht "Pop"), "Hip Hop" und "Rap"
-                if lowercase_genre == "k-pop":
-                    prediction_data.at[0, matching_columns[0]] = 1
-                elif "pop" in matching_columns[0].lower():
-                    # Setze die "Pop"-Spalte nur, wenn das Genre nicht "K-Pop" ist
-                    prediction_data.at[0, matching_columns[0]] = 1
-                elif "hip-hop" in matching_columns[0].lower() or "rap" in matching_columns[0].lower():
-                    # Setze die "Hip Hop" und "Rap"-Spalten nur, wenn das Genre "hip hop" oder "rap" ist
-                    prediction_data.at[0, matching_columns[0]] = 1
+             # Extrahiere die Vorschau-URL des Tracks
+            preview_url = track_data.get('preview_url', None)
+            
+            if preview_url:
+                st.subheader("Audio Preview:")
 
-        
-        # st.subheader("Track Information:")
-        # st.write(f"Name: {track_data['name']}")
-        # st.write(f"Artist: {track_data['artists'][0]['name']}")
-        # st.write(f"Popularity: {track_data['popularity']}")
-        # st.write(f"Release Date: {track_data['album']['release_date']}")
-        # st.write(f"Track ID: {track_data['id']}")
+                # Verwende st.audio mit JavaScript autoplay
+                audio_code = f'<audio controls autoplay><source src="{preview_url}" type="audio/mp3"></audio>'
+                st.markdown(audio_code, unsafe_allow_html=True)
+            else:
+                st.warning("No audio preview available for this track.")
 
-        #st.subheader("Audio Features:")
-        #st.write(audio_features)
-
-        # Laden Sie die gespeicherten Scaler-Parameter
-        with open('scaler.joblib', 'rb') as file:
-            scaler = joblib.load(file)
-
-        # Skaliere die numerischen Features
-        scaled_numeric_features = scaler.transform(numeric_features_data)
-        scaled_numeric_features_df = pd.DataFrame(scaled_numeric_features, columns=numeric_features_data.columns)
-
-        # Hier erfolgt die Verknüpfung
-        final_data = pd.concat([scaled_numeric_features_df, prediction_data], axis=1)
-
-        df = pd.DataFrame(tracks_data)
-        # st.write("Collected Data:")
-        # st.write(final_data)
-
-        # Laden Sie das trainierte Modell
-        with open('model1.pkl', 'rb') as file:
-            trained_model = pickle.load(file)
-
-        # Extrahiere die Features für die Vorhersage
-        features_for_prediction = final_data.values.reshape(1, -1)  # Reshape für eine einzelne Beobachtung
-
-        # Führe die Vorhersage durch
-        predicted_score = trained_model.predict(features_for_prediction)
-
-        # Zeige die Vorhersage an
-        st.subheader("Predicted Popularity Score:")
-        # Wenn kein passendes Genre gefunden wurde, gib eine Warnung aus
-        if prediction_data.iloc[0].sum() == 0:
-            st.error("Be careful with the result! It could be inaccurate because no genre was found for the song that fits the prediction model.", icon="🚨")
-        st.write(predicted_score[0])
-
-        import pickle
-
-        # Lade das zweite Modell
-        loaded_model = pickle.load(open("monthly_listeners_model.pkl", "rb"))
-
-        # Annahme: predicted_score ist der vorhergesagte Wert
-        pred_score = predicted_score
-
-        # Führe die Vorhersage durch
-        predicted_monthly_listeners = loaded_model.predict([pred_score])
-
-        # Berechne zusätzliche Metriken basierend auf der Vorhersage
-        predicted_monthly_streams = predicted_monthly_listeners *2.5
-        predicted_monthly_revenue = predicted_monthly_streams * 0.004
-
-        # Jetzt hast du die vorhergesagten Werte, die du in deinem Hauptcode verwenden kannst
-        st.write("Predicted Monthly Listeners:", predicted_monthly_listeners[0])
-        st.write("Predicted Monthly Streams:", predicted_monthly_streams[0])
-        st.write("Predicted Monthly Revenue:", predicted_monthly_revenue[0])
-        
-    else:
-        st.error("Error getting track information and audio features.")
-
+            
+        else:
+            st.error("Error getting track information and audio features.")
