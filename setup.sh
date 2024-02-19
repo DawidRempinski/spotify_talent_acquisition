3564ea65506ea0f83a9361e1d5aebaedcabfa586
DawidRempinski <dawid@rempinski.de>
2024-02-19 14:59:35 +0100

    changed colors

--- setup.sh
+++ setup.sh
@@ -16,7 +16,7 @@ echo "\
 [theme]\n\
 primaryColor = '#1DB954'\n\
 backgroundColor = '#191414'\n\
-secondaryBackgroundColor = '#7beaa2'\n\
-textColor = '#000000'\n\
+secondaryBackgroundColor = '#191414'\n\
+textColor = '#FFFFFF'\n\
 font = 'sans serif'\n\
 " >> ~/.streamlit/config.toml
