bb6088441d7d319a249fa71f46bc595c65883ec8
DawidRempinski <dawid@rempinski.de>
2024-02-19 14:56:27 +0100

    deleted .streamlit folder and changed setup.sh

--- setup.sh
+++ setup.sh
@@ -10,13 +10,13 @@ echo "\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
-" >> ~/.streamlit/config.toml
+" > ~/.streamlit/config.toml
 
-# echo "\
-# [theme]\n\
-# primaryColor = '#7792E3'\n\
-# backgroundColor = '#e6e6e6'\n\
-# secondaryBackgroundColor = '#7beaa2'\n\
-# textColor = '#000000'\n\
-# font = 'sans serif'\n\
-# " >> ~/.streamlit/config.toml
+echo "\
+[theme]\n\
+primaryColor = '#1DB954'\n\
+backgroundColor = '#191414'\n\
+secondaryBackgroundColor = '#7beaa2'\n\
+textColor = '#000000'\n\
+font = 'sans serif'\n\
+" >> ~/.streamlit/config.toml
