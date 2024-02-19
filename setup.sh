793c2f6af886a82e1d84bfa913f5ba55df7f9119
DawidRempinski <dawid@rempinski.de>
2024-02-19 14:51:44 +0100

    fixed config.toml and setup.sh

--- setup.sh
+++ setup.sh
@@ -5,12 +5,12 @@ echo "\
 email = \"dawid@rempinski.de\"\n\
 " > ~/.streamlit/credentials.toml
 
-# echo "\
-# [server]\n\
-# headless = true\n\
-# enableCORS=false\n\
-# port = $PORT\n\
-# " > ~/.streamlit/config.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" >> ~/.streamlit/config.toml
 
 # echo "\
 # [theme]\n\
